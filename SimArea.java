52551252c721c256b7fbad8a68ab68dd7d8521aa
FraAurCas <frankcasella17@gmail.com>
2020-05-26 08:49:38 -0400

    Made outline of some files
    Did a bad job making a gridpane.
    Made placeholders for the base material, which will have the basic gravity stuff.
        also dirt
    Viscosity Interface
        Will have things for the stackability and movement speeds.

---
+++ SimArea.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.layout.GridPane;
+import javafx.stage.*;
+
+public class SimArea extends Application{
+    public void start(Stage primaryStage){
+        GridPane sim = new GridPane();
+        sim.setGridLinesVisible(true);
+        
+    }
+}
