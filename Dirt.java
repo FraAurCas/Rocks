161f911632904fed5358891a259237415db7aec4
FraAurCas <frankcasella17@gmail.com>
2020-05-28 08:37:48 -0400

    constructor for mat.

--- Dirt.java
+++ Dirt.java
@@ -1,3 +1,8 @@
 public class Dirt extends Material implements Viscosity{
+
+    public Dirt() {
+        super("Dirt", "Brown", 3, 30);
+
+    }
     
 }
--- Material.java
+++ Material.java
@@ -1,3 +1,13 @@
 public class Material {
-    
+    private String name;
+    private String color;
+    private int vHeight;
+    private double vSpeed;
+
+    public Material(String name, String color, int vHeight, double vSpeed){
+        this.name = name;
+        this.color = color;
+        this.vHeight = vHeight;
+        this.vSpeed = vSpeed;
+    }
 }
