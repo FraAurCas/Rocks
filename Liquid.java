d9da808f6967d5eacaeaf91ddfde137e593204f2
FraAurCas <frankcasella17@gmail.com>
2021-06-08 14:02:10 -0400

    Added some planning information
    Various TODOs in Liquid

--- Liquid.java
+++ Liquid.java
@@ -1,5 +1,12 @@
 public interface Liquid{
     public void levelV();
-    
+
 
 }
+
+//Restructure program to make Liquid and LooseSolid classes instead of interfaces
+//Pass distinct values for each material into the appropriate supers
+//Make liquid not teleport (perhaps push other blocks down the row); ensure no movements are more than one pixel
+//Fix crash when a material is added in the pixel of a pixel that is moving
+//Add custom materials
+//Gasses?
